5a265b163ded3c9d39f6ef06a3021710ae57318a
pgorovoy <pgorovoy@playflock.com>
2018-03-22 22:13:20 +0300

    webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
 export default class App extends React.Component {
-    render() {
-        return (
-            <div>
-                lalalla
-            </div>
-        );
-    }
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+    this.timerID = null;
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+        <span>{this.state.date.toLocaleTimeString()}</span>
+        <p>lalal</p>
+      </div>
+    );
+  }
+}
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,68 +1,66 @@
 import path from 'path';
-import HtmlWebpackPlugin from'html-webpack-plugin';
+import fs from 'fs';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 const appHtml = resolveApp('src/index.html');
 
+const extractSass = new ExtractTextPlugin({
+  filename: '[name].css',
+  disable: process.env.NODE_ENV === 'development'
+});
+const cleanDist = new CleanWebpackPlugin(['dist']);
+const connectHtml = new HtmlWebpackPlugin({ inject: true, template: appHtml });
+
+
 module.exports = {
-  mode: 'development',
   entry: { // указываем источники файлов свои
     app: './src/index.js',
   },
   devtool: 'inline-source-map', // при ошибках будет показывать файл-источник, а не просто bunlde.js
   devServer: {
-    contentBase: './dist' // вебпак сервер, тут же настраивается хот-релоад
+    contentBase: './dist' // вебпак сервер, и хот-релоад если нужно
   },
   output: {
-    filename: '[name].bundle.js', // название файла выхода
+    filename: '[name].js', // название файла выхода
     path: path.resolve(__dirname, 'dist'), // путь выхода
-      publicPath: "/" //
+    publicPath: '/' //
+  },
+  resolve: { // Эта штука разрешила заимпортить компоненты
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+    },
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
-      new CleanWebpackPlugin(['dist']), // очищает перед сборкой папку дист
-      new HtmlWebpackPlugin({ // Эта штука для автогенерации в хтмл файл твоих файлов
-          inject: true,
-          template: appHtml
-      })
+    cleanDist, // очищает перед сборкой папку дист
+    connectHtml, // Эта штука для подключения в хтмл чего-либо
+    extractSass // подключаем sass и из
   ],
-    module: { // Подключение загрузчиков, чтобы вебпак мог обрабатывать файлы разных форматов
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: 'html-loader',
-                    options: {
-                        minimize: true
-                    }
-                }]
-            }
-  //     {
-  //       test: /\.css$/,
-  //       use: ['style-loader', 'css-loader'],
-  //     },
-  //     {
-  //       test: /\.(png|svg|jpg|gif)$/,
-  //       use: ['file-loader'],
-  //     },
-  //     {
-  //       test: /\.(woff|woff2|eot|ttf|otf)$/,
-  //       use: ['file-loader'],
-  //     },
-  //     {
-  //       test: /\.(csv|tsv)$/,
-  //       use: ['csv-loader'],
-  //     },
-  //     {
-  //       test: /\.xml$/,
-  //       use: ['xml-loader'],
-  //     },
-    ],
+  module: { // Подключение загрузчиков, чтобы вебпак мог обрабатывать файлы разных форматов
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }],
+          // use style-loader in development
+          fallback: 'style-loader'
+        })
+      }
+    ]
   },
 };
